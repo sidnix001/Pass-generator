892e2d01afe2272be406ee4fc57449824b1e2f6e
Sidnix <vmakisev65@gmail.com>
2025-03-04 10:23:13 +0200

    Add files via upload

---
+++ pass generator/scripts/script.js
@@ -0,0 +1,35 @@
+let outputPass = document.getElementById("outputPass");
+let overlay = document.getElementById("overlay")
+
+const letters = "qwertyuiopasdfghjklzxcvbnm";
+const specialChars = "!@#$%^&*()_)+=-"
+let password = "";
+const passLength = 15;
+
+function generatePass () {
+   password = ""
+   for (let i = 0; i < passLength; i++) {
+      let randomNumberOne = Math.floor(Math.random()*10);
+      let randomNumberTwo = Math.floor(Math.random()*10);
+   
+      if (randomNumberOne == randomNumberTwo) {
+         password += letters[Math.floor(Math.random()*letters.length)].toUpperCase();   
+      } else if (randomNumberOne % 2 == 0) {
+         password += String(Math.floor(Math.random()*9))
+      } else if (randomNumberOne % 3 == 0) {
+         password += specialChars[Math.floor(Math.random()*specialChars.length)]
+      } else {
+         password += letters[Math.floor(Math.random()*letters.length)];
+      }
+      
+      // console.log(password, randomNumberOne)
+   }
+   outputPass.innerHTML = password;
+}
+generatePass()
+
+overlay.addEventListener("click", generatePass);
+
+outputPass.addEventListener("click", () => {
+   navigator.clipboard.writeText((password))
+})
